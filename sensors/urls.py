6b18c18d648dfe81e76c6f12bc3c65d6af1d7b44
jay6294100293 <142631405+jay6294100293@users.noreply.github.com>
2024-02-15 23:44:57 -0800

    new_update
    new update

--- sensors/urls.py
+++ sensors/urls.py
@@ -1,7 +1,8 @@
 # sensor_app/urls.py
 from django.urls import path
-from .views import update_sensor_data
+from .views import update_sensor_data, sensor_data_view
 
 urlpatterns = [
     path('update_sensor_data/', update_sensor_data, name='update_sensor_data'),
+    path('sensor_data/', sensor_data_view, name='sensor_data_view'),
 ]
--- sensors/views.py
+++ sensors/views.py
@@ -3,6 +3,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import SensorData
 from .serializers import SensorDataSerializer
+# sensor_app/views.py
+from django.shortcuts import render
+from .models import SensorData
 
 @api_view(['POST'])
 def update_sensor_data(request):
@@ -14,3 +17,19 @@ def update_sensor_data(request):
         return Response(serializer.data)
     else:
         return Response(serializer.errors, status=400)
+
+
+def sensor_data_view(request):
+    latest_data = SensorData.objects.order_by('-id').first()  # Get the latest data
+
+    risk_message = None
+    if latest_data:
+        if latest_data.temperature > 30:
+            risk_message = "High temperature - increased risk of wildfire!"
+        # Add more conditions for other risk factors if needed
+
+    context = {
+        'latest_data': latest_data,
+        'risk_message': risk_message,
+    }
+    return render(request, 'sensor_app/sensor_data.html', context)
